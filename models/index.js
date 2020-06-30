21dae5db67c6c54110143b6d0d8b262c9b2f4e3f
Samuel Garbo <samuel.garbo@gmail.com>
2020-06-30 21:42:06 +0300

    Add db link to index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-import DB_URL from '../url';
 
 // mongoose.set('debug', true);
 
-const connection = process.env.MONGODB_URI || URL;
+const connection = process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0-uhuud.azure.mongodb.net/todo-api?retryWrites=true&w=majority';
 mongoose.connect(connection,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 .then(() => console.log('Database connected'))
 .catch(err => console.log(err));
