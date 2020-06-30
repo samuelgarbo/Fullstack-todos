e22323c2019afac73883d02d9271c2c834736392
Samuel Garbo <samuel.garbo@gmail.com>
2020-06-30 21:22:07 +0300

    Add postbuild to package.json

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express'),
 app = express(),
 compression = require('compression'),
 helmet = require('helmet'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+const path = require('path');
 
 const PORT =  process.env.PORT || 3001;
 
@@ -13,11 +14,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression());
 app.use(helmet());
 
-app.get('/', function(req, res){
-    res.send('Hi there from express');
-})
+// app.get('/', function(req, res){
+//     res.send('Hi there from express');
+// })
 
 app.use('/api/todos', todoRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(PORT, function(){
     console.log('app is running on port '+PORT)
