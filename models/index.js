9af146999097aec81509bff119b6a56906705a7e
Samuel Garbo <samuel.garbo@gmail.com>
2020-06-30 23:57:32 +0300

    Remove database url

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // mongoose.set('debug', true);
 
-const connection = process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0-uhuud.azure.mongodb.net/todo-api?retryWrites=true&w=majority';
+const connection = process.env.MONGODB_URI ;
 mongoose.connect(connection,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 .then(() => console.log('Database connected'))
 .catch(err => console.log(err));
