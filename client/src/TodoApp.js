63e7fc086da4abcc0917fc70682e4894e91af72f
Samuel Garbo <samuel.garbo@gmail.com>
2020-06-30 23:14:09 +0300

    Remove Toolbar from imports

--- client/src/TodoApp.js
+++ client/src/TodoApp.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-import ToolBar from '@material-ui/core/ToolBar';
+// import Typography from '@material-ui/core/Typography';
+// import ToolBar from '@material-ui/core/ToolBar';
 import Grid from '@material-ui/core/Grid';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
@@ -14,9 +14,7 @@ function TodoApp(props) {
             style={{ padding: 0, margin: 0, height: '100vh', backgroundColor: '#fafafa' }} elevation={0}
         >
             <AppBar color='primary' position='static' style={{ height: '64px' }}>
-                <ToolBar>
-                    <Typography color='inherit'>Todos with hooks</Typography>
-                </ToolBar>
+              
             </AppBar>
             <Grid container justify='center' style={{ marginTop: '1rem' }}>
                 <Grid item xs={11} md={8} lg={4}>
